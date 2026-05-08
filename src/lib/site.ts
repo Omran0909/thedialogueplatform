7f258157e4e5b0e17ca31e00dc5c21d5ac1a8b73
Omran Adam <omranadam@MacBookPro.home>
2026-05-08 12:20:19 +0200

    Update featured dialogue examples

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -124,19 +124,34 @@ export const siteConfig = {
   facebookCommunityImage: "/assets/media/site/library/seminars/silik/2026-01-24/silik-2026-01-24-0784.jpg",
   exampleDialogues: [
     {
-      title: "Example Dialogue 01",
+      title: "Khalid Omer Yousif with Dr. Bakri Aljack",
+      href: "https://www.youtube.com/@thedialogueplattform/search?query=%D8%AE%D8%A7%D9%84%D8%AF%20%D8%B9%D9%85%D8%B1%20%D9%8A%D9%88%D8%B3%D9%81%20%D8%A8%D9%83%D8%B1%D9%8A%20%D8%A7%D9%84%D8%AC%D8%A7%D9%83",
+      thumbnail: "/assets/media/youtube/khalid-bakri-dialogue.png",
+    },
+    {
+      title: "Political Dialogue with Dr. Azzam Abdallah Ibrahim",
       href: "https://youtu.be/eC5fJTXGQsg",
       thumbnail: "https://i.ytimg.com/vi/eC5fJTXGQsg/hqdefault.jpg",
     },
     {
-      title: "Example Dialogue 02",
-      href: "https://youtu.be/hYD4fEoxNv8",
-      thumbnail: "https://i.ytimg.com/vi/hYD4fEoxNv8/hqdefault.jpg",
-    },
-    {
-      title: "Example Dialogue 03",
+      title: "Conflict, Trust, and Peace with Moneim Suleiman Atroun",
       href: "https://youtu.be/NU42C6AANSg",
       thumbnail: "https://i.ytimg.com/vi/NU42C6AANSg/hqdefault.jpg",
+    },
+    {
+      title: "Community Dialogue with Hassan and Moneim Atroun",
+      href: "https://www.youtube.com/@thedialogueplattform/search?query=%D8%AD%D8%B3%D9%86%20%D9%85%D9%86%D8%B9%D9%85%20%D8%B9%D8%B7%D8%B1%D9%88%D9%86",
+      thumbnail: "/assets/media/youtube/community-dialogue-hassan-atroun.png",
+    },
+    {
+      title: "Direct Community Exchange with Gada Ayoub and Atroun",
+      href: "https://www.youtube.com/@thedialogueplattform/search?query=%D8%BA%D8%A7%D8%AF%D8%A9%20%D8%A3%D9%8A%D9%88%D8%A8%20%D9%85%D9%86%D8%B9%D9%85%20%D8%B9%D8%B7%D8%B1%D9%88%D9%86",
+      thumbnail: "/assets/media/youtube/gada-ayoub-atroun-dialogue.png",
+    },
+    {
+      title: "Humanitarian Situation Seminar in Jabal Awliya",
+      href: "https://www.youtube.com/@thedialogueplattform/search?query=%D8%A7%D9%84%D9%88%D8%B6%D8%B9%20%D8%A7%D9%84%D8%A5%D9%86%D8%B3%D8%A7%D9%86%D9%8A%20%D8%AC%D8%A8%D8%A7%D9%84%20%D8%A7%D9%84%D8%A3%D9%88%D9%84%D9%8A%D8%A7%D8%A1",
+      thumbnail: "/assets/media/youtube/humanitarian-situation-seminar.png",
     },
   ],
 };
